5b8f5aafb4276b08a674b886b4a1664b22dc13cf
Binoy Subhakumar <subhakum@usc.edu>
2018-05-19 18:22:16 -0700

    Implemented pool as a generic one removed using object boxing

--- ObjectpoolTest/Assets/Scripts/ObjectPool.cs
+++ ObjectpoolTest/Assets/Scripts/ObjectPool.cs
@@ -1,67 +1,120 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
-public class ObjectPool<T>
+public class ObjectPool<T> where T:class
 {
-    public static ObjectPool<T> current;
     public T pooledObject;
     public int objectsToPreAllocate = 20;
     public int maxNumberOfObjects;
+    public int extrObjectsCount;
     public bool willGrow = true;
-    public Queue<object> pooledObjects;
+    public Queue<T> pooledObjects;
     public Type ObjectType;
+    public object[] parameters;
 
     public bool destroyInProgress { get; private set; }
     
-    public ObjectPool(Type ObjectType, int objectsToPreAllocate,int maxNumberOfObjects, bool willGrow){
+    public ObjectPool(int objectsToPreAllocate,int maxNumberOfObjects,object[] parameters, bool willGrow,GameObject poolobj)
+    {
 
         this.objectsToPreAllocate = objectsToPreAllocate;
         this.maxNumberOfObjects = maxNumberOfObjects;
+        this.parameters = parameters;
         this.willGrow = willGrow;
-        this.ObjectType = ObjectType;
+        this.ObjectType = typeof(T);
         this.destroyInProgress = false;
-
+        extrObjectsCount = maxNumberOfObjects - objectsToPreAllocate;
+        if (poolobj != null)
+        {
+            this.pooledObject = poolobj as T;
+        }
+        
         // Queue which keeps all the queued objects
-        pooledObjects = new Queue<object>();
+        pooledObjects = new Queue<T>();
 
         for(int i=0;i< this.objectsToPreAllocate; i++)
         {
-            createGenericObjectAndAddtoQueue(); 
-        }
-        
-    }
-    // helper method to create a pool object and add to pool
-    private object createGenericObjectAndAddtoQueue()
-    {     
-        // T obj =  (T)Activator.CreateInstance(typeof(T));
-       // object obj = Activator.CreateInstance(TheType,new object[] { 1, 'a' });
-        object obj = Activator.CreateInstance(ObjectType);
-        pooledObjects.Enqueue(obj);
-        return obj;
-            
-    }
-    //helper to get an object from pool
-    public object GetObjectFromPool()
-    {
-        
-            if (getNumberOfObjectsInPool() > 0 && !destroyInProgress)
+            if (ObjectType == typeof(GameObject))
             {
-                return pooledObjects.Dequeue();
-            }
-            else if (willGrow && getNumberOfObjectsInPool() < maxNumberOfObjects && !destroyInProgress)
-            {
-                return createGenericObjectAndAddtoQueue();
+                pooledObjects.Enqueue(pooledObject);
             }
             else
             {
-                //return default(T);
-                return null;
+                pooledObjects.Enqueue(createGenericObject());
             }
+            
+        }
+        
+    }
+
+
+    // helper method to create a pool object and add to pool
+    private T createGenericObject()
+    {
+      
+        T obj;
+        //no need to create an  instance of gameobjects
+        if (ObjectType == typeof(GameObject))
+        {
+            GameObject gameobject = GameObject.Instantiate(pooledObject as GameObject);
+            gameobject.SetActive(true);
+            return gameobject as T;
+        }
+        if (parameters.Length > 0)
+            obj =(T) Activator.CreateInstance(ObjectType,parameters);
+        else
+            obj = (T)Activator.CreateInstance(ObjectType);
+
+        return obj;
+            
+    }
+
+    public void InstantiateGameObjects()
+    {
+        if (ObjectType != typeof(GameObject))
+        {
+            throw new Exception("Method can be invoked only for GameObject pool");
+        }
+        else
+        {
+            for(int i = 0; i < pooledObjects.Count; i++)
+            {
+                GameObject obj = GameObject.Instantiate(pooledObjects.Dequeue() as GameObject);
+                obj.SetActive(false);
+                pooledObjects.Enqueue(obj as T);
+            }
+        }
+
+    }
+
+    //helper to get an object from pool
+    public T GetObjectFromPool()
+    {
+       
+        
+            if (getNumberOfObjectsInPool() > 0 && !destroyInProgress)
+            {
+                T obj= pooledObjects.Dequeue(); ;
+            return obj;
+            }
+            else if (willGrow && extrObjectsCount>0 && !destroyInProgress)
+            {
+                extrObjectsCount--;
+                return createGenericObject();
+                 
+            }
+            else
+            {
+                return default(T);
+               // return null;
+            }
+        
     }
     //helper to add object back to pool
-    public void AddBackToPool(object returningObject)
+    public void AddBackToPool(T returningObject)
     {
         if (!returningObject.Equals(null) && getNumberOfObjectsInPool() < maxNumberOfObjects && !destroyInProgress)
         {
@@ -78,7 +131,7 @@ public class ObjectPool<T>
         destroyInProgress = true;
         while (getNumberOfObjectsInPool() > 0)
         {
-            pooledObjects.Dequeue();
+            pooledObjects.Dequeue(); 
         }
         destroyInProgress = false;
     }
--- ObjectpoolTest/Assets/Scripts/PoolTester.cs
+++ ObjectpoolTest/Assets/Scripts/PoolTester.cs
@@ -2,45 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PoolTester : MonoBehaviour {
 
-	// Use this for initialization
- 
-   
-    public ObjectPool<int> abc;
+    // Use this for initialization
+
+
+    //   public ObjectPool<int> abc;
+    //   private void Awake()
+    //   {
+    //       abc = new ObjectPool<int>(typeof(int), 20,40, true);
+    //   }
+    //   void Start () {
+
+    //       StartCoroutine(destroyAfter(5));
+    //   }
+
+    //// Update is called once per frame
+    //void Update () {
+    //       if (abc.getNumberOfObjectsInPool() > 40)
+    //       {
+    //           abc.willGrow = false;
+    //       }
+    //       int obje = (abc.GetObjectFromPool()) != null ? (int)abc.GetObjectFromPool():0;
+
+    //       //StartCoroutine(waitAndRun(obje));
+
+    //       Debug.Log(obje + " : "+ abc.getNumberOfObjectsInPool());
+    //   }
+    //   public IEnumerator waitAndRun(int obje)
+    //   {
+    //       // WAIT FOR 3 SEC
+    //       yield return new WaitForSeconds(Time.deltaTime*2);
+    //       // RUN YOUR CODE HERE ...
+    //       abc.AddBackToPool(obje);
+    //   }
+    //   public IEnumerator destroyAfter(int seconds)
+    //   {
+    //       // WAIT FOR 3 SEC
+    //       yield return new WaitForSeconds(seconds);
+    //       // RUN YOUR CODE HERE ...
+    //       abc.destroyObjectsinPool();
+    //   }
+
+    public GameObject poolobject;
+    public float upForce = 1f;
+    public float sideForce = 0.1f;
+    ObjectPool<GameObject> pool;
+    Queue<int> a;
     private void Awake()
     {
-        abc = new ObjectPool<int>(typeof(int), 20,40, true);
+      pool = new ObjectPool<GameObject>(20, 40,new object[]{ }, true, poolobject);
+      pool.InstantiateGameObjects();
     }
-    void Start () {
+    private void Update()
+    {
 
-        StartCoroutine(destroyAfter(5));
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        if (abc.getNumberOfObjectsInPool() > 40)
+        float xForce = Random.Range(-sideForce, sideForce);
+        float yForce = Random.Range(upForce/2f, upForce);
+        float zForce = Random.Range(-sideForce, sideForce);
+
+        Vector3 force = new Vector3(xForce, yForce, zForce);
+        GameObject obj = pool.GetObjectFromPool();
+        if (obj != null && !obj.Equals(default(GameObject)))
         {
-            abc.willGrow = false;
+            obj.SetActive(true);
+            obj.transform.position = new Vector3(0f, 0f, 0f);
+            obj.GetComponent<Rigidbody>().velocity = force;
+            Debug.Log("PoolSize: " + pool.getNumberOfObjectsInPool());
+            StartCoroutine(AddBackToPosol(obj, 2f));
         }
-        int obje = (abc.GetObjectFromPool()) != null ? (int)abc.GetObjectFromPool():0;
-       
-        //StartCoroutine(waitAndRun(obje));
-        
-        Debug.Log(obje + " : "+ abc.getNumberOfObjectsInPool());
+
+        //if (obj != null && !obj.Equals(default(GameObject)))
+        //{
+        //    Instantiate(obj);
+        //    obj.SetActive(true);
+        //    obj.GetComponent<Rigidbody>().velocity = force;
+        //    //  StartCoroutine(AddBackToPosol(obj, 2f));
+
+        //}
+
+
     }
-    public IEnumerator waitAndRun(int obje)
+    IEnumerator AddBackToPosol(GameObject poolobject, float delayTime)
     {
-        // WAIT FOR 3 SEC
-        yield return new WaitForSeconds(Time.deltaTime*2);
-        // RUN YOUR CODE HERE ...
-        abc.AddBackToPool(obje);
-    }
-    public IEnumerator destroyAfter(int seconds)
-    {
-        // WAIT FOR 3 SEC
-        yield return new WaitForSeconds(seconds);
-        // RUN YOUR CODE HERE ...
-        abc.destroyObjectsinPool();
+        yield return new WaitForSeconds(delayTime);
+        poolobject.SetActive(false);
+        pool.AddBackToPool(poolobject);
     }
 }
